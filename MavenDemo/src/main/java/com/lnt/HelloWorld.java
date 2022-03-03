3366c5de3dc3dfbe1b68a3af37b3bcae29a9a321
HOME <radhakrishna6624@gamil.com>
2022-03-03 17:15:17 -0500

    second commit

---
+++ MavenDemo/src/main/java/com/lnt/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.lnt;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello...good evening");
+		// TODO Auto-generated method stub
+
+	}
+
+}
